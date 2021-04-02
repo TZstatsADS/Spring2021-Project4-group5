5881b3fd03fca16e188d019ec1f4b0ae4668a985
Amir Idris <aai2125@columbia.edu>
2021-04-02 17:24:56 -0400

    Updated S structure and function for D

--- lib/propensity_matching.R
+++ lib/propensity_matching.R
@@ -2,25 +2,29 @@
 # Project 4 Group 5
 # Created by Amir Idris
 
-# EACH e is a tuple s.t. e = (idx, prop_score, treatment_group_indicator)
+# Each e is a tuple s.t. e = (idx, prop_score, treatment_group_indicator)
 # e.g. e_1 = (5, 0.4, False)
 
+# U will be a matrix where each row corresponds to an e_i
+
 # D is the matrix of pairwise distances between all prop scores in our data
+
+# S will be a list of lists corresponding to the matching assignment
 
 net_discrepancy <- function(S, D){
   total_disc <- 0
   
   for(i in 1:length(S)){
-    curr_group <- S[i]
+    curr_group <- S[[i]]
     for(j in 1:length(curr_group)){
       for(k in 2:length(curr_group)){
         if(j != k){
           
           # Check that groups are opposite
-          if(curr_group[j][3] != curr_group[k][3]){
+          if(curr_group[[j]][3] != curr_group[[k]][3]){
             # Get discrepancy between points
-            idx1 <- curr_group[j][1]
-            idx2 <- curr_group[k][1]
+            idx1 <- curr_group[[j]][1]
+            idx2 <- curr_group[[k]][1]
             d <- D[idx1, idx2]
             total_dis <- total_dis + d
           }
@@ -38,7 +42,7 @@ find_closest_comparison <- function(e, U, D){
   curr_idx <- e[1]
   for(i in 1:dim(D)[1]){
     if(i != curr_idx){
-      curr_e <- U[i]
+      curr_e <- U[i,]
       curr_disc <- D[curr_idx, i]
       if((curr_e[3] != e[3]) & (curr_disc < min_disc)){
         min_disc <- curr_disc
@@ -51,22 +55,33 @@ find_closest_comparison <- function(e, U, D){
   
 }
 
+# Root method for propensity matching
+propensity_matching_root <- function(U, D){
+  
+  S <- list()
+  ans <- propensity_matching( U, S, D)
+  return(ans)
+}
+
 
 propensity_matching <- function(U, S, D){
   # Pop new example from U
-  e <- tail(U,1)
-  new_U <- U[1:length(U) - 1]
+  n <- dim(U)[1]
+  e <- U[n,]
+  new_U <- U[1:n-1,]
   min_discrepancy <- Inf
   best_S <- NULL
   
   for(i in length(S)){
     # Insert e into one of the groups
-    new_s <- append(S[i], e)
+    new_s <- S[[i]]
+    new_s.size <- length(new_s)
+    new_s[[new_s.size + 1]] = e
     new_S <- S
-    new_S[i] <- new_s
+    new_S[[i]] <- new_s
     
     # Recurse on this new matching assignment
-    new_S <- propensity_matching(new_U, new_S)
+    new_S <- propensity_matching(new_U, new_S, D)
     net_dis <- net_discrepancy(new_S, D)
     
     # Check if this is best so far
@@ -79,11 +94,14 @@ propensity_matching <- function(U, S, D){
   
   # Test if putting e in new group will work best
   e_prime <- find_closest_comparison(e, U, D)
-  new_group <- c(e,e_prime)
+  new_group <- list()
+  new_group[[1]] <- e
+  new_group[[2]] <- e_prime
   new_S <- S
-  new_S <- append(new_S, new_group)
+  S.size <- length(new_S)
+  new_S[[S.size + 1]] <- new_group
   
-  new_S <- propensity_matching(U, new_S)
+  new_S <- propensity_matching(U, new_S, D)
   net_dis <- net_discrepancy(new_S, D)
   
   # Check if this is better
